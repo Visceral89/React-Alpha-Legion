52db91879dce26ed38f1814d7be392a948909cd3
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-08-21 12:41:14 +0200

    Navbar Start
    Started working on the navbar and some of the stylings. Unfortunatly its just copying a bunch of stuff for learning this. Reminder. Change the logo svg to white!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from 'react'
 // import { Article, Brand, CTA, Navbar, Feature } from './components';
 import { Footer, Blog, Features, Header, Possibility, WhatAlpha } from './container';
 import { Brand, CTA, Navbar } from './components';
+import "./App.css";
 
 const App = () => {
   return (
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/alpha-legion.svg';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='alpha__navbar'>
+      <div className="alpha__navbar-links">
+        <div className="alpha__navbar-links_logo">
+          <img src={logo} alt="alpha legion logo" />
+        </div>
+      </div>
+    </div>
   )
 }
 
