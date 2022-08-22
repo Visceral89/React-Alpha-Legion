b6c65ef5012a307c95e42ea2eef1755a192147e0
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-08-22 21:40:07 +0200

    Mobile menu.
    Might revert this later felt cute.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLin, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/alpha-legion.svg';
 import './Navbar.css';
 
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#whatalpha">What is the Alpha Legion?</a></p>
+  <p><a href="#primarch">Alpharious</a></p>
+  <p><a href="#primarch">Omegon</a></p>
+  <p><a href="#blog">Read Moore</a></p>
+  </>
+)
+
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className='alpha__navbar'>
       <div className="alpha__navbar-links">
@@ -11,16 +24,25 @@ const Navbar = () => {
           <img src={logo} alt="alpha legion logo" />
         </div>
         <div className="alpha__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#whatalpha">What is the Alpha Legion?</a></p>
-          <p><a href="#primarch">Alpharious</a></p>
-          <p><a href="#primarch">Omegon</a></p>
-          <p><a href="#blog">Read Moore</a></p>
+          <Menu />
         </div>
       </div>
       <div className='alpha__navbar-sign'>
         <p>Sign In</p>
         <button type='button'>Sign Up</button>
+      </div>
+      <div className="alpha__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="alpha__navbar-menu_container scale-up-center">
+            <div className='alpha__navbar-menu_container-links'>
+              <Menu />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
