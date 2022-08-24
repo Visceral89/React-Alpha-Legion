c59ff186cd80ed9eafffc8a50c1d86a032286ce4
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-08-24 23:18:19 +0200

    Started working on what Alpha section

--- src/container/whatAlpha/WhatAlpha.jsx
+++ src/container/whatAlpha/WhatAlpha.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Feature } from '../../components';
 import './WhatAlpha.css';
 
 const WhatAlpha = () => {
   return (
-    <div>WhatAlpha</div>
+    <div>
+      <div className="alpha__whatalpha section__margin" id="whatalpha">
+        <div className="alpha__whatalpha-feature">
+          <Feature />
+        </div>
+        <div className="alpha__whatalpha-heading">
+          <h1 className='gradient-text'>The possibilities are beyond your imagination</h1>
+          <p>Explore the Black Library</p>
+        </div>
+        <div className="alpha__whatalpha-container">
+          <Feature />
+          <Feature />
+          <Feature />
+        </div>
+      </div>
+    
+    </div>
   )
 }
 
