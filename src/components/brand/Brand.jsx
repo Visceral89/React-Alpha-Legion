c96d2f825a44c704ecc9d7dd53fc27be56b19762
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-08-24 22:40:17 +0200

    brand section done!

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { google,slack,shopify,dropbox } from './imports';
 import './Brand.css';
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className="alpha__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/brand/imports.js
@@ -0,0 +1,11 @@
+import google from '../../assets/google.png'
+import dropbox from '../../assets/dropbox.png'
+import shopify from '../../assets/shopify.png'
+import slack from '../../assets/slack.png'
+
+export {
+    google,
+    dropbox,
+    shopify,
+    slack
+}
