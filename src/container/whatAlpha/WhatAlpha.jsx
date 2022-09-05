72c4f75af48d5c1bc4e20d2f1c3fc3f9ed0ca299
Rasmus Bremholm <rasmus.brem@gmail.com>
2022-09-05 14:10:23 +0200

    Update WhatAlpha.jsx

--- src/container/whatAlpha/WhatAlpha.jsx
+++ src/container/whatAlpha/WhatAlpha.jsx
@@ -16,7 +16,7 @@ const WhatAlpha = () => {
         <div className="alpha__whatalpha-container">
           <Feature title="Origins" text="As with most lore, the Alpha Legion's name comes from religion. The two Primarchs, Alpharius and Omegon, represent the line Jesus said in Revelation 22:13: 'I am the Alpha and Omega, the First and the Last, the Beginning and the End.' But as Omegon is meant to be a secret, the legion is named after Alpharius. " />
           <Feature title="Tactics" text="During the Great Crusade the Ultramarines had a proverb: 'Information is victory.' What they meant to say is, 'If you know an opponent, you can defeat them.' The Alpha Legion took this doctrine and turned it up to eleven: why focus only on the intel you have? An enemy that doesn't know you (or better has nothing but carefully crafted disinformation about you) is doomed. That's pretty much what their strategy revolves around: fucking with their opponent's mind." />
-          <Feature title="Leaders" text="\[.../// REDACTED ///...\]" />
+          <Feature title="Leaders" text="[.../// REDACTED ///...]" />
         </div>
       </div>
     
